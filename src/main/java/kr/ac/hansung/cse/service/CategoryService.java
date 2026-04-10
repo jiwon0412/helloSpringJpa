dab960f737202e4df604dc5b04759032ffb08a94
jiwon <jiwon@naver.com>
2026-04-10 22:18:20 +0900

    feat: CategoryService 수정

--- src/main/java/kr/ac/hansung/cse/service/CategoryService.java
+++ src/main/java/kr/ac/hansung/cse/service/CategoryService.java
@@ -11,7 +11,11 @@ import java.util.List;
 @Service
 @Transactional
 public class CategoryService {
-    private CategoryRepository categoryRepository;
+    private final CategoryRepository categoryRepository;
+
+    public CategoryService(CategoryRepository categoryRepository) {
+        this.categoryRepository = categoryRepository;
+    }
 
     public List<Category> getAllCategories() {
         return categoryRepository.findAll();
